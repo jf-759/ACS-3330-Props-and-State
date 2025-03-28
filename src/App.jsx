f7af92d5b0be8b5ce9ab991c2bd20957a38ac507
jf-759 <jane.fernandez@students.dominican.edu>
2025-03-27 23:20:18 -0700

    completed 4 & 5 of Lab 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Counter from './Counter'
 import './App.css'
 
 function App() {
   const [counts, setCounts] = useState([1, 4, 3])
 
+
+  const updateCount = (index, delta) => {
+    setCounts(prevCounts =>
+      prevCounts.map((count, i) => (i === index ? count + delta : count))
+    )
+  }
+
+  const totalCount = counts.reduce((sum, count) => sum + count, 0);
+
   return (
     <div className="App">
+      <h2>Total Count: {totalCount}</h2>
       {counts.map((value, index) => (
-        <Counter key={index} label={`Counter ${index + 1}`} value={value} />
+        <Counter 
+        key={index} 
+        label={`Counter ${index + 1}`} 
+        value={value}
+        increment={() => updateCount(index, 1)}
+        decrement={() => updateCount(index, -1)} 
+        />
       ))}
     </div>
   )
