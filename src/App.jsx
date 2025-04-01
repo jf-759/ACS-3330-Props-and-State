004b5e3a475d5fc973642f4a33e9109ea2017ec8
jf-759 <jane.fernandez@students.dominican.edu>
2025-03-31 23:38:04 -0700

    added stretch challenges

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,44 @@ function App() {
 
 
   const updateCount = (index, delta) => {
-    setCounts(prevCounts =>
-      prevCounts.map((count, i) => (i === index ? count + delta : count))
-    )
+    const newCounts = [...counts]
+    if (newCounts[index] + delta >= 0 && newCounts[index] + delta <= 10) {
+      newCounts[index] += delta
+    }
+    setCounts(newCounts)
   }
 
-  const totalCount = counts.reduce((sum, count) => sum + count, 0);
+  const totalCount = counts.reduce((sum, count) => sum + count, 0)
   const resetCounts = () => {
-    setCounts([0, 0, 0])
+    setCounts(counts.map(() => 0))
+  }
+
+  const addCounter = () => {
+    setCounts([...counts, 0])
+  }
+
+  const removeCounter = (index) => {
+    const newCounts = counts.filter((_, i) => i !== index)
+    setCounts(newCounts)
   }
 
   return (
     <div className="App">
-      <h2>Total Count: {totalCount}</h2>
+      <h2 className='total'>Total Count: {totalCount}</h2>
       {counts.map((value, index) => (
         <Counter 
         key={index} 
         label={`Counter ${index + 1}`} 
         value={value}
         increment={() => updateCount(index, 1)}
-        decrement={() => updateCount(index, -1)} 
+        decrement={() => updateCount(index, -1)}
+        remove={() => removeCounter(index)} 
         />
       ))}
-      <button onClick={resetCounts}>Reset</button>
+      <div className="button">
+        <button onClick={resetCounts} className="reset-button">Reset</button>
+        <button onClick={addCounter} className="add-button">➕ Add Counter</button>
+      </div>
     </div>
   )
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -6,6 +6,7 @@ function Counter(props) {
             <h1>{props.value}</h1>
             <button onClick={props.increment}>+</button>
             <button onClick={props.decrement}>-</button>
+            <button onClick={props.remove}>➖ Remove Counter</button>
         </div>
     )
 }
